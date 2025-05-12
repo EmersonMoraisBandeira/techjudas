ed4c3629ec18056b01623a0790175dfff0f93736
Pedro de Paula Oliveira Lessa - 82324880 <82324880@ulife.com.br>
2025-05-12 20:46:35 -0300

    Entrega03(front): finaliza interaface grafica

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import 'primereact/resources/themes/bootstrap4-light-purple/theme.css';
+import { Button } from 'primereact/button';
+
+import './App.css';
+import logo from './assets/componentes/logo.png';
+
+import Busca from './assets/componentes/busca';
+import AgendaSolidaria from './assets/componentes/AgendaSolidaria';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [telaAtual, setTelaAtual] = useState('busca');
+
+  const irParaAgenda = () => setTelaAtual('agenda');
+  const voltarParaBusca = () => setTelaAtual('busca');
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid justify-content-center m-auto w-9 border-round">
+      <div className="logo-container">
+        <div className="logo">
+        <img src={logo} alt="Logo" className="logo-img" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      {telaAtual === 'busca' && (
+  <div className="flex align-items-center justify-content-center min-h-screen">
+    <div className="card-container text-center">
+      <div className="col-12">
+        <h1 className="titulo-busca">Qual a causa que você deseja apoiar?</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="col-12 flex justify-content-center mt-3">
+        <Busca />
+      </div>
+      <div className="col-12 mt-4">
+        <Button label="Ir para a Agenda Solidária" onClick={irParaAgenda} className="p-button-info" />
+      </div>
+    </div>
+  </div>
+)}
+
+
+      {telaAtual === 'agenda' && (
+        <div className="col-12">
+          <AgendaSolidaria voltar={voltarParaBusca} />
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
